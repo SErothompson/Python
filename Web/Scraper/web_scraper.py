70b8a6e7c6d8b7a2094eb21f2b0367f07dc6538e
Robert Thompson <se.rothompson@gmail.com>
2024-11-27 00:15:50 -0500

    Updating the code

--- Web/Scraper/web_scraper.py
+++ Web/Scraper/web_scraper.py
@@ -1,18 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://webscraper.io/test-sites"  # Replace with the URL you want to scrape
-response = requests.get(url)
+url = "https://www.uoguide.com/Bane"  # Replace with the URL you want to scrape
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
 
-# Check if the request was successful
+response = requests.get(url, headers=headers)
+
 if response.status_code == 200:
     page_content = response.text
+    soup = BeautifulSoup(page_content, 'html.parser')
+    
+    links = soup.find_all('a')
+    for link in links:
+        href = link.get('href')
+        print(href)
 else:
     print(f"Failed to retrieve the page. Status code: {response.status_code}")
-
-soup = BeautifulSoup(page_content, 'html.parser')
-
-links = soup.find_all('a')
-for link in links:
-    href = link.get('href')
-    print(href)
